f2c4c39ef9586d4a41dc03cf63fbdaefb44bfcab
Alexei Crecotun <crecotun@gmail.com>
2019-02-09 12:54:56 +0100

    Initial
    Nextjs
    TypeScript
    Prettier

---
+++ src/scenes/Main/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Button from 'src/components/Button'
+
+const Main: React.FunctionComponent = () => {
+  return (
+    <div>
+      Custom React Boilerplate <br />
+      <Button />
+    </div>
+  )
+}
+
+export default Main
